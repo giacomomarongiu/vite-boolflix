c8c16adc096e7cfc72ac773c1ae0c1b47222d43b
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-05 16:36:59 +0200

    [Refactoring] AppMain -> Clean from checkboxes + AppHeader -> Add Genre LIst

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,11 +14,46 @@ export default {
     <header class="">
         <div class="nav container">
             <h2 class="logo">BOOLFLIX</h2>
+
+
+            <!--Filtri per genere-->
+
+            <!--Filtri per film-->
+            <div class="wrapper">
+                <div class="genre"> Movies Category</div>
+                <div class="movies">
+                    <div class="movie" v-for="(movie_type, index) in state.moviesGenres">
+                        <input type="checkbox" :id="movie_type" :value="movie_type" v-model="state.moviesFilter"
+                            @change="state.filterByGenre()">
+                        <label :for="movie_type"> {{ movie_type }} </label>
+                    </div>
+                </div>
+            </div>
+            <!--/Filtri per film-->
+
+            <br>
+
+            <!--Filtri per serieTV-->
+            <div class="wrapper">
+                <div class="genre">TV Shows</div>
+                <div class="series">
+                    <div class="serie" v-for="(series_type, index) in state.seriesGenres">
+                        <input type="checkbox" :id="series_type" :value="series_type" v-model="state.seriesFilter"
+                            @change="state.filterByGenre()">
+                        <label :for="series_type"> {{ series_type }} </label>
+                    </div>
+                </div>
+            </div>
+            <!--/Filtri per serieTV-->
+
+            <!--/Filtri per genere-->
+
+
             <div class="search">
                 <!--v-model mi permette di aggiornare la stringa searchedString nello state-->
                 <!--Al @enter chiamo la funzione searchProduct()-->
-                <input class="searchTerm" type="text" placeholder="Cerca uno show..." v-model="this.state.searchedString"
-                    @keyup.enter="this.state.searchProduct()">
+                <input class="searchTerm" type="text" placeholder="Cerca uno show..."
+                    v-model="this.state.searchedString" @keyup.enter="this.state.searchProduct()">
             </div>
         </div>
     </header>
@@ -42,15 +77,40 @@ header {
             font-size: large;
             font-weight: bolder;
         }
-            .searchTerm {
-                width: 100%;
-                border: 2px solid #ff0202;
-                padding: 5px;
-                height: 20px;
-                border-radius: 5px;
-                outline: none;
-                color: #0c0000;
+
+        .searchTerm {
+            border: 1px solid #ff0202;
+            padding: 5px;
+            height: 20px;
+            border-radius: 5px;
+            outline: none;
+            color: #0c0000;
+        }
+
+        .wrapper {
+            position: relative;
+
+            .movies, .series {
+                position: absolute;
+                display: none;
+                height: 100px;
+                width: 350px;
+                background-color: #ff0202;
+                color: black;
+                flex-direction: column;
+                flex-wrap: wrap;
             }
+        }
+
+        .wrapper:hover .movies {
+            display: flex;
+        }
+
+        .wrapper:hover .series {
+            display: flex;
+            right: 0;
+        }
+
     }
 }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -24,38 +24,6 @@ export default {
 <template>
     <main class=" ">
         <div class="container">
-
-            <!--Filtri per genere-->
-            <div class="filters container">
-
-                <!--Filtri per film-->
-                <h2> Movies:</h2>
-                <div class="movies">
-                    <div class="movie" v-for="(movie_type, index) in state.moviesGenres">
-                        <input type="checkbox" :id="movie_type" :value="movie_type" v-model="state.moviesFilter"
-                            @change="state.filterByGenre()">
-                        <label :for="movie_type"> {{ movie_type }} </label>
-                    </div>
-                </div>
-                <!--/Filtri per film-->
-
-                <br>
-
-                <!--Filtri per serieTV-->
-                <h2>TV Shows:</h2>
-                <div class="series">
-                    <div class="serie" v-for="(series_type, index) in state.seriesGenres">
-                        <input type="checkbox" :id="series_type" :value="series_type" v-model="state.seriesFilter"
-                            @change="state.filterByGenre()">
-                        <label :for="series_type"> {{ series_type }} </label>
-                    </div>
-                </div>
-                <!--/Filtri per serieTV-->
-
-            </div>
-            <!--/Filtri per genere-->
-
-
             <div class="row" v-if="this.state.filteredProducts.length > 0">
                 <ProductCard :product="product" v-for="product in state.filteredProducts" />
             </div>
@@ -65,25 +33,5 @@ export default {
 </template>
 
 <style>
-.filters {
-    padding: 2%;
-        text-align: center;
 
-    >* {
-        display: flex;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        >* {
-            padding: 1px;
-            min-width: 33%;
-        }
-    }
-     h2{
-        background-color: black;
-        color:red;
-        margin-bottom: 1rem;
-    }
-
-}
 </style>
