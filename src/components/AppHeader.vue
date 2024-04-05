4fa512db0d7e61b80b0698ce7c0081aa22ce039f
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-05 15:19:35 +0200

    [Refactoring] Start cleaning Component by Component -> AppHeader now has all its improved style on the page + Add commets

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,11 +13,12 @@ export default {
 <template>
     <header class="">
         <div class="nav container">
-            <div class="logo">BOOLFLIX</div>
-            <div id="search-bar">
-                <span><input type="text" placeholder="Cerca un Film..." v-model="this.state.searchedString"
-                        @keyup.enter="this.state.searchProduct()"></span>
-                <span><button @click="this.state.searchProduct()">Cerca</button></span>
+            <h2 class="logo">BOOLFLIX</h2>
+            <div class="search">
+                <!--v-model mi permette di aggiornare la stringa searchedString nello state-->
+                <!--Al @enter chiamo la funzione searchProduct()-->
+                <input class="searchTerm" type="text" placeholder="Cerca uno show..." v-model="this.state.searchedString"
+                    @keyup.enter="this.state.searchProduct()">
             </div>
         </div>
     </header>
@@ -25,5 +26,31 @@ export default {
 </template>
 
 <style>
+header {
+    height: 40px;
+    background-color: black;
+    color: red;
+    display: flex;
+    align-items: center;
 
+    .nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .logo {
+            font-size: large;
+            font-weight: bolder;
+        }
+            .searchTerm {
+                width: 100%;
+                border: 2px solid #ff0202;
+                padding: 5px;
+                height: 20px;
+                border-radius: 5px;
+                outline: none;
+                color: #0c0000;
+            }
+    }
+}
 </style>
